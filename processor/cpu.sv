a5507071342d926536cedb2aac98e6ee16555c38
Shirley <shirleyx.zhou@mail.utoronto.ca>
2019-03-03 21:18:18 -0500

    Update cpu.sv

--- processor/cpu.sv
+++ processor/cpu.sv
@@ -68,7 +68,7 @@ pc my_pc(
     .clk(clk),
     .reset(reset),
     .enable(pc_enable & br_cond), // enable branch , next pc_out = in + 2
-	.incr(fetch & (~busy)),
+	.incr(fetch),
     .i_addr(pc_in),
     .pc_out(pc_out)
 	//.pc_nxt(pc_nxt)
@@ -106,9 +106,7 @@ opcode_decoder my_control(
 	.mem_sel(mem_sel),
 	.BSrc(BSrc),
 	.pc_enable(pc_enable),
-	.fetch(fetch),
-	.BrCond(br_sel), // 0 = always br(no condition) , 1 = branch if Z == 1, 2 = branch if N == 1
-	.busy(busy)
+	.BrCond(br_sel) // 0 = always br(no condition) , 1 = branch if Z == 1, 2 = branch if N == 1
 );
 
 
